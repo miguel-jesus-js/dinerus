c5748ed5ddc3995a099822f568d00f344e496c11
Aldair Antonio <a.antonio@cyberpuerta.mx>
2024-03-21 22:39:47 -0600

    Mejoras

---
+++ database/migrations/2024_03_11_022629_create_tickets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\User::class);
+            $table->string('reference')->nullable();
+            $table->string('shift')->nullable();
+            $table->boolean('paid')->nullable();
+            $table->string('voucher')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
